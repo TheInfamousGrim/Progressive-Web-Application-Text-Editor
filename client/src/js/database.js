5cf8581cd94ffae4a887c36f799ea268f152e256
TheInfamousGrim <finchergeorge1@gmail.com>
2022-12-03 05:54:19 +0000

    Build: update config file for webpack
    Added and configured the webpack plugins to generate a service worker
    and manifest file.
    
    Added CSS loader and babel modules to import css and transpile ES6 and
    above to ES5 compliant code. n.b. even though I think I'm not going to
    have to worry about this as internet explorer is now dead 🙏

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,16 +1,16 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+    openDB('jate', 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains('jate')) {
+                console.log('jate database already exists');
+                return;
+            }
+            db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+            console.log('jate database created');
+        },
+    });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
